920abda4b0ae3b279c90a8806386575f777ccee6
Sascha <sascha.wombacher@gmx.de>
2015-12-02 18:24:55 +0100

    added output, renamed 'Grad' to 'degree' in Polynom.hpp

--- p2/include/Polynom.hpp
+++ p2/include/Polynom.hpp
@@ -1,10 +1,13 @@
+#pragma once
 
+#include <iostream>
 #include <ostream>
 #include <array>
 
-template<int _MaxGrad, int _BaseValue>
+
+template<int _MaxDegree, int _BaseValue>
 class Polynom{
-    typedef Polynom<_MaxGrad, _BaseValue> MY_TYPE;
+    typedef Polynom<_MaxDegree, _BaseValue> MY_TYPE;
 public:
     Polynom(){
         for (auto& e : this->m_Data)
@@ -17,7 +20,7 @@ public:
         this->m_Data = std::move(rhs.m_Data);
     }
     Polynom(const std::initializer_list<int>& list) {
-        if (list.size() != _MaxGrad)
+        if (list.size() != _MaxDegree)
             throw "Error: Initializer list has to many arguments (in function Polynom(std::initializer_list<int>)";
 
         int counter = list.size() - 1;
@@ -29,7 +32,7 @@ public:
 
 
     MY_TYPE& operator+=(const MY_TYPE& poly){
-        for (unsigned int i = 0; i < _MaxGrad; ++i)
+        for (unsigned int i = 0; i < _MaxDegree; ++i)
             this->m_Data[i] += poly.m_Data[i];
 
         this->_truncateToBaseValue();
@@ -37,7 +40,7 @@ public:
     }
     MY_TYPE operator+(const MY_TYPE& poly){
         MY_TYPE toReturn;
-        for (unsigned int i = 0; i < _MaxGrad; ++i)
+        for (unsigned int i = 0; i < _MaxDegree; ++i)
             toReturn.m_Data[i] = this->m_Data[i] + poly.m_Data[i];
 
         toReturn._truncateToBaseValue();
@@ -50,67 +53,147 @@ public:
 
         // create temporary polynom with 2*_MaxLength
         // this is the maximum grad the *-Operation can create
-        Polynom<_MaxGrad * 2, _BaseValue> tmp;
+        Polynom<_MaxDegree * 2, _BaseValue> tmp;
 
         // calculate "multiplication" into tmp poly
-        for (int i = 0, myGrad = this->grad(), polyGrad = poly.grad(); i <= myGrad; ++i){
-            for (int k = 0; k <= polyGrad; ++k)
+        for (int i = 0, myDegree = this->degree(), polyDegree = poly.degree(); i <= myDegree; ++i){
+            for (int k = 0; k <= polyDegree; ++k)
                 tmp.m_Data[i + k] += poly.m_Data[k] * this->m_Data[i];
         }
         tmp._truncateToBaseValue();
 
-        // calculate poly division until tmp.grad() <= _MaxGrad
-        const int divisionGrad      = DIVISION_POLYNOM->grad();
-        const int divisionGradKoeff = DIVISION_POLYNOM->m_Data[divisionGrad];
-        int tmpGrad;
-        while( (tmpGrad = tmp.grad())  >  divisionGrad     ||
-              tmp.m_Data[divisionGrad] >= divisionGradKoeff)
-        {
-            int shift = tmpGrad - divisionGrad;
+        // calculate poly division until tmp.degree() <= _MaxDegree
+        const int divisionDegree = DIVISION_POLYNOM->degree();
+
+        while(tmp > *DIVISION_POLYNOM) {
+            int tmpDegree = tmp.degree();
+            int shift = tmpDegree - divisionDegree;
             int factor= 1;
 
             // NOTE:
-            // if factor is always 1 -> factor = tmp.m_Data[tmpGrad];    is faster!
-            while (tmp.m_Data[tmpGrad] != ((factor * DIVISION_POLYNOM->m_Data[divisionGrad]) % _BaseValue) )
+            // if factor is always 1 -> factor = tmp.m_Data[tmpDegree];    is faster!
+            while (tmp.m_Data[tmpDegree] != ((factor * DIVISION_POLYNOM->m_Data[divisionDegree]) % _BaseValue) )
                 ++factor;
 
-            for (int i = 0; i < _MaxGrad; ++i)
+            for (int i = 0; i < _MaxDegree; ++i)
                 tmp.m_Data[shift + i] -= factor * DIVISION_POLYNOM->m_Data[i];
 
             tmp._truncateToBaseValue();
         }
         MY_TYPE toReturn;
-        for (int i = 0; i < _MaxGrad; ++i)
+        for (int i = 0; i < _MaxDegree; ++i)
             toReturn.m_Data[i] = tmp.m_Data[i];
 
         return toReturn;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const MY_TYPE& poly){
+        bool alreadyWrote = false;
         for (int i = poly.m_Data.size() - 1; i >= 0; --i){
-            //os << poly.m_Data[i] << "*x^" << i << (i ? "  + " : "");
-            os << poly.m_Data[i];
-            if (i)
-                os << "*x^" << i << "  + ";
+            if (!poly.m_Data[i]){
+                if (alreadyWrote || i)
+                    continue;
+            }
 
+            if (alreadyWrote)
+                os << '+';
+
+            alreadyWrote = true;
+            if (poly.m_Data[i] > 1 || i == 0)
+                os << poly.m_Data[i];
+            if (i == 1)
+                os << "x";
+            else if (i > 1)
+                os << "x" << i;
+            else{
+            }
         }
-
         return os;
     }
 
     MY_TYPE& operator=(MY_TYPE&& poly){
         this->m_Data = std::move(poly.m_Data);
+        this->m_Degree = std::move(poly.m_Degree);
         return *this;
     }
 
-    int grad() const {
-        int grad = 0;
-        for (int i = 0; i < this->m_Data.size(); ++i){
-            if (this->m_Data[i])
-                grad = i;
+    int degree() const {
+        if (this->m_Degree < 0){
+            // degree has been adjusted
+            int degree = -1;
+            for (int i = this->m_Data.size() - 1; i >= 0; --i){
+                if (this->m_Data[i]){
+                    degree = i;
+                    break;
+                }
+            }
+            this->m_Degree = degree;
         }
-        return grad;
+        return this->m_Degree;
     }
+
+//private:
+    template<int v, int k>
+    friend class Polynom;
+
+    template<int _RhsMaxDegree>
+    int compare(const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        int degreeIter = this->degree();
+        if (degreeIter < rhs.degree())
+            return -1;
+        if (degreeIter > rhs.degree())
+            return +1;
+
+        while (degreeIter >= 0){
+            if (this->m_Data[degreeIter] < rhs.m_Data[degreeIter])
+                return -1;
+            if (this->m_Data[degreeIter] > rhs.m_Data[degreeIter])
+                return +1;
+
+            --degreeIter;
+        }
+        return 0;
+    }
+
+    template<int _RhsMaxDegree>
+    bool operator< (const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) == -1)
+            return true;
+        return false;
+    }
+    template<int _RhsMaxDegree>
+    bool operator> (const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) == 1)
+            return true;
+        return false;
+    }
+
+    template<int _RhsMaxDegree>
+    bool operator>=(const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) >= 0)
+            return true;
+        return false;
+    }
+    template<int _RhsMaxDegree>
+    bool operator<=(const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) <= 0)
+            return true;
+        return false;
+    }
+
+    template<int _RhsMaxDegree>
+    bool operator==(const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) == 0)
+            return true;
+        return false;
+    }
+    template<int _RhsMaxDegree>
+    bool operator!=(const Polynom<_RhsMaxDegree, _BaseValue>& rhs){
+        if (this->compare(rhs) != 0)
+            return true;
+        return false;
+    }
+
 
     void _truncateToBaseValue(){
         for (auto& e : this->m_Data){
@@ -118,10 +201,12 @@ public:
             if (e < 0)
                 e += _BaseValue;
         }
+        this->m_Degree = -1;
     }
-    std::array<int, _MaxGrad> m_Data;
+    mutable int m_Degree = -1;
+    std::array<int, _MaxDegree> m_Data;
 };
 
 
-template<int _MaxGrad, int _BaseValue>
-const Polynom<_MaxGrad, _BaseValue>* Polynom<_MaxGrad, _BaseValue>::DIVISION_POLYNOM = nullptr;
+template<int _MaxDegree, int _BaseValue>
+const Polynom<_MaxDegree, _BaseValue>* Polynom<_MaxDegree, _BaseValue>::DIVISION_POLYNOM = nullptr;
--- p2/main.cpp
+++ p2/main.cpp
@@ -1,12 +1,13 @@
 
 #include "Polynom.hpp"
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char** argv){
-    Polynom<2 + 1, 3> poly1 = { 1, 2, 2 };
+    Polynom<2 + 1, 3> poly1 = {0, 1, 0}; //{ 1, 1, 1 };
     std::cout << "First   polynom: \r\t\t\t" << poly1 << std::endl;
 
-    Polynom<2 + 1, 3> poly2 = { 0, 0, 1 };
+    Polynom<2 + 1, 3> poly2 = {1, 1, 1};//{ 0, 0, 1 };
     std::cout << "Second  polynom: \r\t\t\t" << poly2 << std::endl;
 
     Polynom<2 + 1, 3> divisionPoly = { 1, 1, 1 };
@@ -15,6 +16,193 @@ int main(int argc, char** argv){
 
     poly1 = poly1 * poly2;
     std::cout << std::endl << std::endl << "Poly1 * Poly2: \r\t\t\t" << poly1 << std::endl << std::endl;
+
+    std::cout << "Press enter to continue" << std::endl;
+    getchar();
+    system("clear");
+
+
+    // F_8, aka F_(2³)
+    constexpr int F23_value = 8;
+    constexpr int F23_space = 9;
+    // check for error
+    auto generatePolynom23 = [](int value) -> Polynom<3, 2>{
+        constexpr int base = 2;
+        Polynom<3, 2> toReturn;
+        for (int i = 0; value; ++i){
+            toReturn.m_Data[i] = value % base;
+            value /= base;
+        }
+        return toReturn;
+    };
+    Polynom<3, 2> div23 = { 1, 1, 1 };
+    Polynom<3, 2>::DIVISION_POLYNOM = &div23;
+
+    for (int iter1 = 0; iter1 < F23_value; ++iter1){
+        auto poly_a = generatePolynom23(iter1);
+
+    for (int iter2 = 0; iter2 < F23_value; ++iter2){
+        auto poly_b = generatePolynom23(iter2);
+
+    for (int iter3 = 0; iter3 < F23_value; ++iter3){
+        auto poly_c = generatePolynom23(iter3);
+        if (((poly_a * poly_b) * poly_c) != (poly_a * (poly_b * poly_c)) )
+            throw "Error: wrong implementation";
+    }}}
+
+    std::cout << "multiplication found no errors" << std::endl << "Press enter to continue";
+    getchar();
+    system("clear");
+
+
+    // addition TABLE
+    // create table header
+    std::cout << "Addition table: F_(2³)" << std::endl;
+    for (int iter = 0; iter < F23_value; ++iter){
+        auto poly = generatePolynom23(iter);
+        std::cout << '\r' << std::string(iter + 1, '\t') << '|' << poly;
+    }
+    std::cout << std::endl << std::string(80 - F23_space, '-') << std::endl;
+
+    // table itself
+    for (int iter1 = 0; iter1 < F23_value; ++iter1){
+
+        // create table line
+        std::cout << std::string(80 - F23_space, '-') << std::endl;
+
+        auto poly_a = generatePolynom23(iter1);
+        std::cout << poly_a << "\r\t\b|";
+
+    for (int iter2 = 0; iter2 < F23_value; ++iter2){
+        auto poly_b = generatePolynom23(iter2);
+
+        std::cout << '\r' << std::string(iter2 + 1, '\t') << '|' << poly_a + poly_b;
+    }
+    std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl << std::endl;
+    std::cout << "Press enter to continue";
+    getchar();
+    system("clear");
+
+
+    // multiplication TABLE
+    // create table header
+    std::cout << "Multiplication table: F_(2³)" << std::endl;
+    for (int iter = 0; iter < F23_value; ++iter){
+        auto poly = generatePolynom23(iter);
+        std::cout << '\r' << std::string(iter + 1, '\t') << '|' << poly;
+    }
+    std::cout << std::endl << std::string(80 - F23_space, '-') << std::endl;
+
+    // table itself
+    for (int iter1 = 0; iter1 < F23_value; ++iter1){
+
+        // create table line
+        std::cout << std::string(80 - F23_space, '-') << std::endl;
+
+        auto poly_a = generatePolynom23(iter1);
+        std::cout << poly_a << "\r\t\b|";
+
+    for (int iter2 = 0; iter2 < F23_value; ++iter2){
+        auto poly_b = generatePolynom23(iter2);
+
+        std::cout << '\r' << std::string(iter2 + 1, '\t') << '|' << poly_a * poly_b;
+    }
+    std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+    std::cout << "Press enter to continue";
+    getchar();
+    system("clear");
+
+
+    // F_9, aka F_(3²)
+    constexpr int F32_value = 9;
+    constexpr int F32_space = 11;
+    // check for error
+    auto generatePolynom32 = [](int value) -> Polynom<2, 3>{
+        constexpr int base = 3;
+        Polynom<2, 3> toReturn;
+        for (int i = 0; value; ++i){
+            toReturn.m_Data[i] = value % base;
+            value /= base;
+        }
+        return toReturn;
+    };
+    Polynom<2, 3> div32 = { 2, 2 };
+    Polynom<2, 3>::DIVISION_POLYNOM = &div32;
+
+    // addition TABLE
+    // create table header
+    // this table requires more space -> ignore first column
+    std::cout << "Addition table: F_(3²)" << std::endl;
+    for (int iter = 0; iter < F32_value; ++iter){
+        auto poly = generatePolynom32(iter);
+        if (iter == 0)
+            std::cout << poly;
+        else
+            std::cout << '\r' << std::string(iter, '\t') << '|' << poly;
+    }
+    std::cout << std::endl << std::string(80 - F32_space, '-') << std::endl;
+
+    // table itself
+    for (int iter1 = 0; iter1 < F32_value; ++iter1){
+
+        // create table line
+        std::cout << std::string(80 - F32_space, '-') << std::endl;
+
+        auto poly_a = generatePolynom32(iter1);
+        //std::cout << poly_a << "\r\t\b|";
+
+    for (int iter2 = 0; iter2 < F32_value; ++iter2){
+        auto poly_b = generatePolynom32(iter2);
+        if (iter2 == 0)
+            std::cout << '\r' << poly_a + poly_b;
+        else
+            std::cout << '\r' << std::string(iter2, '\t') << '|' << poly_a + poly_b;
+    }
+    std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+    std::cout << "Press enter to continue";
+    getchar();
+
+
+    // multiplication TABLE
+    // create table header
+    // this table requires more space -> ignore first column
+    std::cout << "Multiplication table: F_(3²)" << std::endl;
+    for (int iter = 0; iter < F32_value; ++iter){
+        auto poly = generatePolynom32(iter);
+        if (iter == 0)
+            std::cout << poly;
+        else
+            std::cout << '\r' << std::string(iter, '\t') << '|' << poly;
+    }
+    std::cout << std::endl << std::string(80 - F32_space, '-') << std::endl;
+
+    // table itself
+    for (int iter1 = 0; iter1 < F32_value; ++iter1){
+
+        // create table line
+        std::cout << std::string(80 - F32_space, '-') << std::endl;
+
+        auto poly_a = generatePolynom32(iter1);
+        //std::cout << poly_a << "\r\t\b|";
+
+    for (int iter2 = 0; iter2 < F32_value; ++iter2){
+        auto poly_b = generatePolynom32(iter2);
+        if (iter2 == 0)
+            std::cout << '\r' << poly_a * poly_b;
+        else
+            std::cout << '\r' << std::string(iter2, '\t') << '|' << poly_a * poly_b;
+    }
+    std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+    std::cout << "Press enter end program";
+    getchar();
+
 	return 0;
 }
-
